c0121a962bf8271efaa925bf998781f10763f4d6
vxldemort69 <34858620+vxldemort69@users.noreply.github.com>
2019-11-09 15:28:29 +0530

    create main

---
+++ main.py
@@ -0,0 +1,13 @@
+from addition import add
+from subtraction import sub
+from multiplication import multi
+from division import div
+from modulo import mod
+
+print ("a=6, b=4")
+
+print ("sum:", add(6, 4))
+print ("difference:", sub(6, 4))
+print ("product:", multi(6, 4))
+print ("quotient:", div(6, 4))
+print ("remainder:", mod(6, 4))
